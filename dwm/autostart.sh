dfc14fbe7a74963069608f1778b5bea8c9af3d07
danalexandru <danb.alexandru@gmail.com>
2022-04-03 18:58:17 +0300

    add: Added "xcompmgr" for st transparency

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -4,6 +4,8 @@ echo "Set alt shift as a way of switching between keyboard layouts"
 
 setxkbmap -layout us,ro -option grp:alt_shift_toggle
 
-/home/aldan/.config/suckless/dwmblocks/dwmblocks
+xcompmgr & # for the terminal transparency
+/home/aldan/.config/suckless/dwmblocks/dwmblocks &
+
 echo "Finished running autostart"
 
