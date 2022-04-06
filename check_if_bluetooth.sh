565e116eb053ca7fdef9a11f1ac4d2bea16c1f5e
danalexandru <danb.alexandru@gmail.com>
2022-04-06 21:30:57 +0300

    add: Added schipt that returns bluetooth icon (connected or disconnected)

---
+++ check_if_bluetooth.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BLUETOOTH_HEADPHONES_MACADDR="38:18:4C:D1:7F:7E"
+CONNECTED=$(bluetoothctl -- info $BLUETOOTH_HEADPHONES_MACADDR | grep Connected)
+
+CONNECTED="$(echo $CONNECTED | tr -d '\t')"
+
+if [ "$CONNECTED" = "Connected: no" ]; then
+    echo ""
+else
+    echo ""
+fi
+
