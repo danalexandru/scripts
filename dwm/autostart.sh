1cea10167d5b4afdc80aa4a26ae5338c64e49a9e
danalexandru <danb.alexandru@gmail.com>
2022-10-11 20:30:55 +0300

    add: Added xinitrc and autostart.sh
    REASON: moved to using a desktop manager => had to port my xinitrc to
    the autostart script

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,6 +6,19 @@ setxkbmap -layout us,ro -option grp:alt_shift_toggle
 
 ${HOME}/.config/scripts/toggle-transparency.sh &
 ${HOME}/.config/suckless/dwmblocks/dwmblocks &
+${HOME}/.screenlayout/layout-ct.sh
+
+udiskie &
+flameshot& 
+xrdb -load ~/.Xresources
+
+nitrogen --restore
+xset s off
+xset -dpms
+xset s noblank
+
+# Turn CapsLock off
+setxkbmap -option ctrl:nocaps &
 
 echo "Finished running autostart"
 
