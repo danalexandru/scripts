c8b44786cfe3de35e476bdd4afd44705327b89e0
danalexandru <danb.alexandru@gmail.com>
2022-10-13 19:54:51 +0300

    add: Added script for compiling suckless software after clean install

---
+++ setup_suckless_software.sh
@@ -0,0 +1,37 @@
+#/!bin/sh
+#
+# This script will be used in order to recompile the suckless software installed on the system, in case of a OS reinstall
+
+# Specify the user (this script will be run as root => you need the absolute path to the suckless locations
+HOMEDIR="/home/REPLACEME"
+
+
+echo "Set up dwm"
+cd ${HOMEDIR}/.config/suckless/dwm
+make clean install
+
+
+echo "Set up st"
+cd ${HOMEDIR}/.config/suckless/st
+make clean install
+
+
+echo "Set up tabbed"
+cd ${HOMEDIR}/.config/suckless/tabbed
+make clean install
+
+
+echo "Set up dmenu"
+cd ${HOMEDIR}/.config/suckless/dmenu
+make clean install
+
+
+echo "Set up dwmblocks"
+cd ${HOMEDIR}/.config/suckless/dwmblocks
+make clean install
+
+
+echo "Set up slock"
+cd ${HOMEDIR}/.config/suckless/slock
+make clean install
+
