b6a61a8c9b13708e26bb98f78fc896ad6edaec6c
Dan Alexandru-Bogdan <danb.alexandru@gmail.com>
2022-12-09 13:31:22 +0200

    Add additional packages to package_list.txt (#1)
    * update: Added additional packages to package_list.txt
    
    * add: Add additional files useful when reinstalling Arch

---
+++ archinstall/systemd_enable.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Enable all systemd daemons after installing required packages
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable cronie
+systemctl enable cronie.service
